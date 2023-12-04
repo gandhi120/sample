a24f26ff105c159d9ce0df08888780602dde72ca
varun-gandhi-digiqc <varun@digiqc.com>
2023-12-04 21:18:31 +0530

    -set icon in ios.

--- src/components/CustomIcon/index.js
+++ src/components/CustomIcon/index.js
@@ -1,19 +1,21 @@
 import {createIconSetFromIcoMoon} from 'react-native-vector-icons';
 import sampleIconConfig from '@theme/sample.json';
+import {Platform} from 'react-native';
 // import PropTypes from 'prop-types';
 
-// const CustomIcon = props => {
-//   if (props.type === 'sample') {
-//     return <Sample {...props} />;
-//   }
-// };
+const Sample = createIconSetFromIcoMoon(
+  sampleIconConfig,
+  Platform.OS === 'android' ? 'sample' : '',
+  Platform.OS === 'android' ? 'sample.ttf' : '',
+);
+const CustomIcon = props => {
+  if (props.type === 'sample') {
+    return <Sample {...props} />;
+  }
+};
 
 // CustomIcon.propTypes = {
 //   type: PropTypes.string,
 // };
 
-export default createIconSetFromIcoMoon(
-  sampleIconConfig,
-  'sample',
-  'sample.ttf',
-);
+export default CustomIcon;
